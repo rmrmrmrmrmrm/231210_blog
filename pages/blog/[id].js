02bd899ac01c613907bd8b10df0e36bb9232f1ef
｛masuda｝ <supika5555@gmail.com>
2023-12-10 01:40:57 +0900

    initial commit

---
+++ pages/blog/[id].js
@@ -0,0 +1,37 @@
+import { client } from "../../libs/client";
+import styles from "../../styles/Home.module.scss";
+
+//SSG
+export const getStaticProps = async (context) => {
+  //urlからid取得
+  const id = context.params.id;
+  const data = await client.get({ endpoint: "blog", contentId: id });
+
+  return {
+    props: {
+      blog: data,
+    },
+  };
+};
+
+// ダイナミックルーティングに必要な設定 SSG設定
+export const getStaticPaths = async () => {
+  //公式ドキュメント（getStaticPaths）https://nextjs.org/docs/pages/building-your-application/data-fetching/get-static-paths
+  const data = await client.get({ endpoint: "blog" });
+  const paths = data.contents.map((content) => `/blog/${content.id}`);
+  return {
+    paths,
+    fallback: false, //パスがなければ404を表示。これがないとエラーになる
+  };
+};
+
+export default function BlogId({ blog }) {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.title}>{blog.title}</h1>
+      <p className={styles.publishedAt}>{blog.publishedAt}</p>
+      {/* HTMLを整形 */}
+      <div className={styles.post} dangerouslySetInnerHTML={{ __html: `${blog.body}` }}></div>
+    </main>
+  );
+}
